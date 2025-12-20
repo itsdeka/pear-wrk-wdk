6c2af85a2e578dd23882170d676c750da73aa7a9
Dario Moceri <dario.moceri@bitfinex.com>
2025-12-20 11:36:09 +0100

    add HRPC to export

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // Export a react-native-bare-kit compatible bundle that generates on postinstall.
 export { default as bundle } from './generated/bundle/wdk-worklet.mobile.bundle.mjs'
+export { default as HRPC } from './src/hrpc/index.js'
