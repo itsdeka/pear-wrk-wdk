94da3041575e2b3c37094e5a1b32a5f91c23fcdf
Dario Moceri <dario.moceri@bitfinex.com>
2025-12-20 11:38:20 +0100

    fix

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-// Export a react-native-bare-kit compatible bundle that generates on postinstall.
-export { default as bundle } from './generated/bundle/wdk-worklet.mobile.bundle.mjs'
+export { default as bundle } from './generated/bundle/wdk-worklet.mobile.bundle.js'
 export { default as HRPC } from './src/hrpc/index.js'
